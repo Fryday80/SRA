f5252a1eaafc4f8f6f8321d8179e4c6c81879ecc
Fryday80 <Fry.lmu@gmx.de>
2016-04-30 15:51:34 +0200

    test commit 2

--- all.inc.php
+++ all.inc.php
@@ -1,6 +1,6 @@
 <?php
 if (basename($_SERVER['SCRIPT_FILENAME']) === 'all.inc.php'){exit('This page may not be called directly !'); }
-//sers
+//sers new auth
 include_once 'inc/functions.inc.php';
 include_once 'inc/db.connect.php';
 include_once 'inc/module.inc.php';
