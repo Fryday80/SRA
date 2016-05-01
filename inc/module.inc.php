e792cad5365938ded728b78baf115f334d18f813
salt42 <boluuuu@gmail.com>
2016-05-01 02:11:01 +0200

    -add file exists check

--- inc/module.inc.php
+++ inc/module.inc.php
@@ -2,11 +2,11 @@
 if (basename($_SERVER['SCRIPT_FILENAME']) === 'module.inc.php'){exit('This page may not be called directly !'); }
 
 $handle = opendir('module/');
-	while (($file = readdir($handle)) !== false)
-	{
-		if ($file != "." && $file != "..")
-		{
-			include ('module/'.$file.'/'.$file.'.cs.php');
-		}
+while (($file = readdir($handle)) !== false) {
+	if ($file != "." && $file != "..") {
+		$path = 'module/' . $file . '/' . $file . '.cs.php';
+		if (file_exists($path))
+			include($path);
 	}
-	closedir($handle);
+}
+closedir($handle);
